dee314944646c7b8940d8695362f344d73b44395
scastro12 <sambigboy@aol.com>
2021-09-01 13:36:16 -0400

    added button to increse age

--- React/my-first-project/src/components/PersonCard.jsx
+++ React/my-first-project/src/components/PersonCard.jsx
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
     
     
 class PersonCard extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    increaseAge =() => {
+        this.setState({ age: this.state.age + 1 });
+
+    }
+
     render() {
         return (
             <>
             <div className="personList">
                 <h3>{this.props.lastName}, {this.props.firstName}</h3>
-                <h5>Age: {this.props.age}</h5>
+                <h5>Age: {this.state.age}</h5>
                 <h5>Hair Color: {this.props.hairColor}</h5>
+                <button onClick={this.increaseAge}>Birthdate Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
             </>
             )
